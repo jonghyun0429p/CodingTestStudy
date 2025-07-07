3abf0ac14a6600e8558b48b75c79af01ad390b42
jonghyun0429p <bwhdgus0429@naver.com>
2025-07-07 22:55:42 +0900

    07.07 구현

--- solved/silver2/18111.py
+++ solved/silver2/18111.py
@@ -1,11 +1,39 @@
 import sys
 input = sys.stdin.readline
 
-#구현하고자 하는 방안.
-#들어오는 수를 리스트화함.
-#ex 000
-#   001  =>  4211 0의 갯수, 1의 갯수... 이런식으로
-#   123
-#그렇게 되면, 최대갯수를 구할 수 있고, time 비교도 비교적 쉬워짐.
-def average(ground, inventory):
+N, M, B = map(int, input().split())
+ground_count_list = [0 for _ in range(257)]
+ground_list = []
+
+for _ in range(N):
+    for ground in map(int, input().split()):
+        ground_count_list[ground] += 1
+        ground_list.append(ground)
+
+def ground_equal(goal, ground_list, inventory):
     time = 0
+    for i in range(len(ground_list)):
+        differ = goal - ground_list[i]
+        if differ < 0:
+            inventory += -differ
+            time -= differ * 2
+        else:
+            time += differ
+            inventory -= differ
+    if inventory < 0:
+        return False
+    return time
+
+goal_ground = 0
+time_min = float('inf')
+
+# ✅ 높은 높이부터 순회
+for count in range(256, -1, -1):
+    time = ground_equal(count, ground_list, B)
+    if time != False:
+        if time < time_min:
+            time_min = time
+            goal_ground = count
+        # 같은 시간이면 높은 count를 유지하므로 따로 조건 필요 없음
+
+print(time_min, goal_ground)
