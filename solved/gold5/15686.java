2e00f3f221b387da90cb9f3d39046719cabd623c
jonghyun0429p <bwhdgus0429@naver.com>
2025-07-17 20:45:42 +0900

    07.17 알고리즘 백트래킹

--- solved/gold5/15686.java
+++ solved/gold5/15686.java
@@ -1,93 +1,73 @@
-//DP
 import java.io.*;
 import java.util.*;
 
 public class Main {
-
-    static int RIGHT = 0;
-    static int DIAGONAL = 1;
-    static int DOWN = 2;
-    static int[][] board;
-
-
+	
+	//치킨집위치를 모두 기억한다면, 그 좌표와 치킨집의 거리를 각각 구하고 치킨 거리 계산 가능.
+	//치킨집 수를 알려주네 M개만 남기고 나머지는 지워야하는데, M개를 지워보면서 각각 구해야하는건가 조합으로?
+	//최대 13개면 최악의 경우의 수는 13C6인 1716이게 되고, 이 갯수만큼 다시 구해야하는데, 크기가 50x50까지 가능
+	//그러면 집이 최대 2N개 그러면 100개 까지 가능
+	//1716*100 약 2000*100 = 200,000번 연산이면 할만 하네.
+	
+	static int[][] village;
+	static List<int[]> homes;
+	static List<int[]> stores;
+	static int N, M;
+   
     public static void main(String[] args) throws IOException {
-
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-        int size = Integer.parseInt(br.readLine());
-
-        board = new int[size][size];
-        int[][][] dp = new int[size][size][3];
-
-        for(int i = 0; i < size; i++){
-            board[i] = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+       
+        int[] NandM = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+        N = NandM[0];
+        M = NandM[1];
+        village = new int[N][N];
+        homes = new ArrayList<>();
+        stores = new ArrayList<>();
+        List<int[]> store = new ArrayList<>();
+       
+        for(int i = 0; i < N; i++) {
+        	int[] line = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+        	for(int j = 0; j < N; j++) {
+        		int cur = line[j];
+        		if(cur == 1) {
+        			homes.add(new int[] {i, j});
+        		}else if(cur == 2) {
+        			stores.add(new int[] {i, j});
+        		}
+        	}
         }
-
-        //start point is 1,2. end point is size, size. how to go end point. let's count the number of ways to move
-        dp[0][1][RIGHT] = 1;
-
-        for(int i = 0; i < size; i++){
-            for(int j = 0; j < size; j++){
-                for(int k = 0; k < 3; k++){
-                    if(dp[i][j][k] > 0){
-                        if(k == RIGHT){
-                            if(canMove(i, j, RIGHT)){
-                                dp[i][j+1][RIGHT] += dp[i][j][RIGHT];
-                            }
-                            if(canMove(i, j, DIAGONAL)){
-                                dp[i+1][j+1][DIAGONAL] += dp[i][j][RIGHT];
-                            }
-
-                        }else if(k == DOWN){
-                           if(canMove(i, j, DOWN)){
-                               dp[i+1][j][DOWN] += dp[i][j][DOWN];
-                           }
-                           if(canMove(i, j, DIAGONAL)){
-                               dp[i+1][j+1][DIAGONAL] += dp[i][j][DOWN];
-                           }
-                        }else{
-                            if(canMove(i, j, DOWN)){
-                                dp[i+1][j][DOWN] += dp[i][j][DIAGONAL];
-                            }
-                            if(canMove(i, j, RIGHT)){
-                                dp[i][j+1][RIGHT] += dp[i][j][DIAGONAL];
-                            }
-                            if(canMove(i, j, DIAGONAL)){
-                                dp[i+1][j+1][DIAGONAL] += dp[i][j][DIAGONAL];
-                            }
-                        }
-                    }
-                }
-            }
-        }
-
-        System.out.println(dp[size-1][size-1][DOWN] + dp[size-1][size-1][RIGHT] + dp[size-1][size-1][DIAGONAL]);
-
-
-
+       
+        System.out.println(backtracking(0, 0, store));        
     }
-
-    public static boolean canMove(int col, int row, int arrow){
-        int size = board.length;
-        if(arrow == RIGHT){
-            if(row+1 < size){
-                if(board[col][row+1] == 0){
-                    return true;
-                }
-            }
-        }else if(arrow == DOWN){
-            if(col+1 < size){
-                if(board[col+1][row] == 0){
-                    return true;
-                }
-            }
-        }else if(arrow == DIAGONAL){
-            if(row+1 < size && col+1 < size){
-                if(board[col][row+1] == 0 && board[col+1][row] == 0 && board[col+1][row+1] == 0){
-                    return true;
-                }
-            }
+   
+    public static int backtracking(int depth, int start, List<int[]> selected) {
+    	
+        if (depth == M) {
+            return getChickenDist(selected);
         }
-        return false;
+
+        int min = Integer.MAX_VALUE;
+
+        for (int i = start; i < stores.size(); i++) {
+            selected.add(stores.get(i));
+            min = Math.min(min, backtracking(depth + 1, i + 1, selected));
+            selected.remove(selected.size() - 1);
+        }
+
+        return min;
+    }
+   
+    static int getChickenDist(List<int[]> store) {
+    	int result = 0;
+    	int minDis;
+    	for(int i = 0; i < homes.size(); i++) {
+    		minDis = Integer.MAX_VALUE;
+    		for(int j = 0; j < store.size(); j++) {
+    			minDis = Math.min(minDis, Math.abs(homes.get(i)[0] - store.get(j)[0]) + Math.abs(homes.get(i)[1] - store.get(j)[1]));
+    		}
+    		result += minDis;
+    	}
+    	
+    	return result;
     }
 }
