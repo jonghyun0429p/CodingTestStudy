bdee3823f512fa7d7248dcc1fc407192f7dbb657
Jonghyeon0429p <bwhdgus0429@naver.com>
2025-09-16 14:22:50 +0900

    feat: 0916 algorithm math

---
+++ solved/bronze4/10156.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int K = Integer.parseInt(st.nextToken());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        if(K*N-M > 0){
+            System.out.println(Math.abs(K*N-M));
+        }else{
+            System.out.println(0);
+        }
+    }
+}
