1fc9b2146742c446ba61dac437b3e4fc7551d9df
bwhdgus0429 <bwhdgus0429@naver.com>
2025-10-17 09:56:34 +0900

    feat:10.17 algorithm bfs

--- solved/gold3/16236.java
+++ solved/gold3/16236.java
@@ -1,88 +1,112 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
+import java.util.ArrayDeque;
 import java.util.Arrays;
+import java.util.Queue;
+import java.util.StringTokenizer;
 
 public class Main{
-	static int[][] visited;
-	static int[][] board;
+	
+	static int size = 2;
+	static int N, time, count;
 	static int[] shark;
+	static int[][] board;
+	static boolean[][] visited;
+	static int[] dx = {0, -1, 0, 1};
+	static int[] dy = {-1, 0, 1, 0};
+	
+	
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        time = 0;
+        count = 0;
+        N = Integer.parseInt(br.readLine());
+        
+        board = new int[N][N];
+        
+        int now;
+        
+        for(int i = 0; i < N; i++) {
+        	String[] line = br.readLine().split(" ");
+        	for(int j = 0; j < N; j++) {
+        		now = Integer.parseInt(line[j]);        		
+        		if(now == 9) {
+        			shark = new int[] {j, i};
+        		}else {
+            		board[i][j] = now;        			
+        		}
+        	}
+        }
+        
+        boolean canEat = true;
+        
+        while(canEat == true) {
+    		visited = new boolean[N][N];
+        	canEat = bfs();
+        }
+        
+        System.out.println(time);
+    }
+    
+    static boolean bfs() {
+        ArrayDeque<int[]> q = new ArrayDeque<>();
+        visited = new boolean[N][N];
 
-	public static void main(String[] args) throws IOException{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        q.offer(new int[]{shark[0], shark[1], 0});
+        visited[shark[1]][shark[0]] = true;
 
-		int N = Integer.parseInt(br.readLine());
-		board = new int[N][N];
+        int minDist = Integer.MAX_VALUE;
+        int targetX = -1, targetY = -1;
 
+        while (!q.isEmpty()) {
+            int[] cur = q.poll();
+            int x = cur[0], y = cur[1], d = cur[2];
 
-		for(int i = 0; i < N; i++) {
-			board[i] = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
-			for(int j = 0; j < N; j++) {
-				if(board[i][j] == 9) {
-					shark = new int[] {j, i};
-				}
-			}
-		}
+            if (d > minDist) break;
 
-		System.out.println(countTime());
-	}
+            for (int k = 0; k < 4; k++) {
+                int nx = x + dx[k];
+                int ny = y + dy[k];
+                int nd = d + 1;
 
-	static int countTime() {
-		int size = 2;
-		int count = 0;
-		int preMovingTime = -1;
-		int movingTime = 0;
+                if (nx < 0 || ny < 0 || nx >= N || ny >= N) continue;
+                if (visited[ny][nx]) continue;
 
-		while(preMovingTime != movingTime) {
-			preMovingTime = movingTime;
-			movingTime += getMovingTime(size);
-			count += 1;
+                if (board[ny][nx] > size) continue;
 
-			if(size == count) {
-				size += 1;
-				count = 0;
-			}
-		}
+                visited[ny][nx] = true;
 
-		return movingTime;
-	}
+                if (board[ny][nx] > 0 && board[ny][nx] < size) {
+                    if (nd < minDist) {
+                        minDist = nd;
+                        targetX = nx; targetY = ny;
+                    } else if (nd == minDist) {
+                        if (ny < targetY || (ny == targetY && nx < targetX)) {
+                            targetX = nx; targetY = ny;
+                        }
+                    }
+                }
+                if (nd <= minDist) {
+                    q.offer(new int[]{nx, ny, nd});
+                }
+            }
+        }
 
-	static int getMovingTime(int size) {
-		int movingTime = 0;
-		int[] fish = getFish(size);
-		System.out.println("먹이 " + fish[0]+","+fish[1]);
-		System.out.println("상어 " + shark[0]+ ","+shark[1]);
-		if(fish[0] != -1 && fish[1] != -1) {
-			movingTime += getMove(int size);
-			shark = fish;
-		}
-		System.out.println(movingTime);
-		return movingTime;
-	}
+        if (minDist == Integer.MAX_VALUE) {
+            return false;
+        }
 
-	static int[] getFish(int size) {
-		int[] fish = new int[2];
-		Arrays.fill(fish, -1);
+        time += minDist;
+        shark = new int[]{targetX, targetY};
+        board[targetY][targetX] = 0;
+        count++;
+        if (count == size) {
+            size++;
+            count = 0;
+        }
+        return true;
+    }
 
-		for(int i = 0; i < board.length; i++) {
-			for(int j = 0; j < board[0].length; j++) {
-				if(0 < board[i][j] && board[i][j] < size) {
-					if(fish[0] == -1 && fish[1] == -1) {
-						fish = new int[] {j, i};
-					}else {
-						int fishDist = Math.abs(fish[0] - shark[0]) + Math.abs(fish[1]-shark[1]);
-						int newDist = Math.abs(i-shark[0]) + Math.abs(j-shark[1]);
-
-						fish = fishDist > newDist ? new int[] {j, i} : fish;
-					}
-				}
-			}
-		}
-		return fish;
-	}
-
-	static int getmove(int size) {
-		Queue<int[]>
-	}
 }
