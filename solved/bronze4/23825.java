a09745144fa9916a04dc20516b62a159e05aa2e3
Jonghyeon0429p <bwhdgus0429@naver.com>
2026-01-23 21:32:14 +0900

    feat: 01.23 algorithm Math

---
+++ solved/bronze4/23825.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        int result = Math.min(N / 2, M / 2);
+        System.out.println(result);
+    }
+}
